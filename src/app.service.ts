88d279df3a77dbb912a5090798a0f71e9251573b
Alexandretecsomobi <alexandredev.frontend@gmail.com>
2023-05-26 19:56:09 -0300

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import { route } from "./router";
+
+export const app = express();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  express.json()(req, res, next);
+});
+app.use(cors());
+app.use(route);
+
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import { app } from "./app.service";
+app.listen(3000, () => console.log("online"));
