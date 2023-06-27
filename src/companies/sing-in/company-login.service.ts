fb2a0d1f6f0155401dbf71df89184c228bfa535b
Alexandretecsomobi <alexandredev.frontend@gmail.com>
2023-06-26 22:11:28 -0300

    version sem ads

--- src/companies/sing-in/company-login.service.ts
+++ src/companies/sing-in/company-login.service.ts
@@ -21,7 +21,9 @@ class SinginCompanyService {
       data: {
         email: email,
         name_company: isUser.name_company,
+        companyId: isUser?.id
       },
+
     });
     return registerSingin;
   }
