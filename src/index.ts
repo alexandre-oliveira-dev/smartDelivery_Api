f1a74909ecd0a8413dec3cc30ce75bd40d6aac41
Alexandretecsomobi <alexandredev.frontend@gmail.com>
2023-09-02 13:50:53 -0300

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { app } from "./app.service";
 
-app.listen(process.env.PORT, () => console.log("online"));
-/* app.listen(3333, () => console.log("online"));
- */
+/*  */app.listen(process.env.PORT, () => console.log("online"));
+app.listen(3333, () => console.log("online"));
