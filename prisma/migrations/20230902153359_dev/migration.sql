ba7d5fb81c2ddb2cf70fe3f420e9e15665afecab
Alexandretecsomobi <alexandredev.frontend@gmail.com>
2023-09-11 18:23:00 -0300

    add news fields

--- prisma/migrations/20230902153359_dev/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "orders" ADD COLUMN     "details" TEXT;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 import { app } from "./app.service";
-app.listen(process.env.PORT, () => console.log("online"));
+app.listen(3333, () => console.log("online"));
